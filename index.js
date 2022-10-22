a57ccd0c7c464fcfd8bf3057b8cefb18503bab43
aliftareq <aliftareq@gmail.com>
2022-10-23 00:14:22 +0600

    connect with vercel server

--- index.js
+++ index.js
@@ -16,9 +16,29 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const catagory_news = news.filter(n => n.category_id === id)
+        res.send(catagory_news)
+    }
+
+})
+
 app.get('/news', (req, res) => {
     res.send(news)
 })
+
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
 
 app.listen(port, () => {
     console.log(`Times news running on port : ${port}`);
